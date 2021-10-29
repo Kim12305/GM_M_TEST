a9096bf4766a63dae46e464b283036db98ced2eb
20201302 김유진 <78015118+Kim12305@users.noreply.github.com>
2021-10-29 02:17:25 +0000

    6.

--- Game.cpp
+++ Game.cpp
@@ -7,7 +7,7 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
   if (m_pWindow != 0) {
   m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
   if (m_pRenderer != 0) {
-  SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
+  SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
   } else {
   return false; // 랜더러 생성 실패
   }
@@ -18,10 +18,10 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
   return false; // SDL 초기화 실패
   }
 
+/*
   SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
   m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
   SDL_FreeSurface(pTempSurface);
-/*
   SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
 
   m_destinationRectangle.w = m_sourceRectangle.w;
@@ -30,20 +30,18 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
   m_destinationRectangle.x = m_sourceRectangle.x = 0;
   m_destinationRectangle.y = m_sourceRectangle.y = 0;
 */
+  SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
+  m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+  SDL_FreeSurface(pTempSurface);
 
-  SDL_QueryTexture(m_pTexture,NULL,NULL,&m_sourceRectangle.w,&m_sourceRectangle.h);
-
-  m_sourceRectangle.x = 0;
-  m_sourceRectangle.y = 0;
+  m_sourceRectangle.w = 128;
+  m_sourceRectangle.h = 82;
 
   m_destinationRectangle.w = m_sourceRectangle.w;
-
   m_destinationRectangle.h = m_sourceRectangle.h;
 
-  m_destinationRectangle.x = 0;
-  m_destinationRectangle.y = 0;
-
-
+  m_destinationRectangle.x = m_sourceRectangle.x = 0;
+  m_destinationRectangle.y = m_sourceRectangle.y = 0;
 
   m_bRunning = true;
   return true;
@@ -51,12 +49,13 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
 
 void Game::update()
 {
+  m_sourceRectangle.x = 128* ((SDL_GetTicks() / 100) % 6);
 }
 
 void Game::render()
 {
   SDL_RenderClear(m_pRenderer);
-  SDL_RenderCopy(m_pRenderer, m_pTexture, NULL, NULL);
+  SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
   SDL_RenderPresent(m_pRenderer);
 }
 
