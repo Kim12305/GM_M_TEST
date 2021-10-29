84d260bd739516db914c4bb48b36faca1fe3e4c4
20201302 김유진 <78015118+Kim12305@users.noreply.github.com>
2021-10-29 02:19:28 +0000

    7.

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "SDL_image.h"
 
 bool Game::init(const char *title, int xpos, int ypos, int width, int height, int flags)
 {
@@ -7,7 +8,7 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
   if (m_pWindow != 0) {
   m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
   if (m_pRenderer != 0) {
-  SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
+  SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
   } else {
   return false; // 랜더러 생성 실패
   }
@@ -18,19 +19,8 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, in
   return false; // SDL 초기화 실패
   }
 
-/*
-  SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/rider.bmp");
-  m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-  SDL_FreeSurface(pTempSurface);
-  SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+  SDL_Surface* pTempSurface = IMG_Load("Assets/animate-alpha.png");
 
-  m_destinationRectangle.w = m_sourceRectangle.w;
-  m_destinationRectangle.h = m_sourceRectangle.h;
-
-  m_destinationRectangle.x = m_sourceRectangle.x = 0;
-  m_destinationRectangle.y = m_sourceRectangle.y = 0;
-*/
-  SDL_Surface* pTempSurface = SDL_LoadBMP("Assets/animate.bmp");
   m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
   SDL_FreeSurface(pTempSurface);
 
